b40121b3e14e56c3e4dc09548910d482e8534268
HelaPortal <92334057+HelaPortal@users.noreply.github.com>
2021-10-11 14:32:00 -0600

    Upload Files

---
+++ js/names.js
@@ -0,0 +1,31 @@
+export const namesOne = {
+    0: "Code",
+    1: "Learning",
+    2: "Programming",
+    3: "Program",
+    4: "Coder",
+    5: "Developer",
+    6: "Developing",
+    7: "Tech",
+    8: "Web",
+    9: "Dev",
+    10: "Algorithms",
+    11: "AR",
+    12: "Development"
+}
+
+export const namesTwo = {
+    0: "Life",
+    1: "MadeEasy",
+    2: "Programmer",
+    3: "World",
+    4: "Data",
+    5: "Stacker",
+    6: "Media",
+    7: "Loader",
+    8: "Simplified",
+    9: "Planet",
+    10: "Master",
+    11: "King",
+    12: "Docker"
+}
